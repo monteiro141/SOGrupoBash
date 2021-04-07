62d82005f4a4c41e4a0fa907c63338722ec123f3
YozNacks <duarte.a.arribas@gmail.com>
2021-04-07 13:33:01 +0100

    fixed removeDirectory bugs

--- removeFromDirectory.sh
+++ removeFromDirectory.sh
@@ -10,14 +10,12 @@ done < "$dbFileLocation/database.db"
 while read lineBackup
 do
   if [[ "$(echo $lineBackup | cut -d ' ' -f 3)" = "$1" ]];then
-    cat "$dbFileLocation/backup.db" | cut -d ' ' -f 4 | grep -vw ^$1$ > backupTemp
     backupFileName=$(echo $lineBackup | cut -d ' ' -f 3 | rev | cut -d '/' -f 1 | rev)
-    echo $backupFileName
-    echo "$backupDir$backupFileName"
     rm -rf "$backupDir$backupFileName.tar.bz2"
-    mv backupTemp "$dbFileLocation/backup.db"
-    rm lastBackup
-    touch lastBackup
-    break
+  else
+    echo "$lineBackup" >> backupTemp
   fi
 done < "$dbFileLocation/backup.db"
+mv backupTemp "$dbFileLocation/backup.db"
+rm lastBackup &> /dev/null
+touch lastBackup &> /dev/null
