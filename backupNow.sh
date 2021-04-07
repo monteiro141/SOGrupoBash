c0416e341833e65bef2b056759b5e9d2e351379e
YozNacks <duarte.a.arribas@gmail.com>
2021-04-07 14:21:06 +0100

    added loading bar

--- backupNow.sh
+++ backupNow.sh
@@ -14,8 +14,13 @@ if [ $? -eq 2 ];then
   mkdir $dbFileLocation
 fi
 
+DIALOG=${DIALOG=dialog}
+let a=$(wc -l "$dbFileLocation/database.db" | cut -d ' ' -f 1)
+COUNT=`expr 100 / $a`
+(
 while read lineDB
 do
+  echo $COUNT
   fileName=$(echo $lineDB | rev | cut -d "/" -f 1 | rev)
   fType=$(file $lineDB)
   if [ $(echo $fType | cut -d " " -f 2) = "directory" ];then
@@ -43,8 +48,11 @@ do
       rm "$backupDir$fileName.tar.bz2"
     fi
     echo "$fileType $filemd5Sum  $(date +"%d/%m/%y|%H:%M:%S") $backupPeriod" >> "$dbFileLocation/backup.db"
+    
     tar cf "$backupDir$fileName.tar" -C $(echo $lineDB | rev | cut -d '/' -f 2- | rev) $(echo $lineDB | rev | cut -d '/' -f 1 | rev)
     bzip2 "$backupDir$fileName.tar"
     echo "$(date +"%d/%m/%y|%H:%M:%S")" > lastBackup
   fi
+  COUNT=`expr $COUNT + 100 / $a`
 done < "$dbFileLocation/database.db"
+) | $DIALOG --title "Backing up your files" --gauge "BACKING UP..." 20 70 0
