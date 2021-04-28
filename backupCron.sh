4bc1a3d07145f5ae25af40939d43e9c5254c551d
YozNacks <duarte.a.arribas@gmail.com>
2021-04-28 18:22:49 +0100

    updated crontab

--- backupCron.sh
+++ backupCron.sh
@@ -3,5 +3,8 @@
 # BackupCron: Runs crontab to backup data
 # ----------------------------------------------------- #
 
+#read default variables from the config file
+. ./src/readConfig.sh
+
 cd "$(crontab -l | cut -d " " -f 7)"
 ./src/backupNow.sh
