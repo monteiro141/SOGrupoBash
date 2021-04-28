590abd623e92c0899e33efc1a27f1b1c7eda1fbc
Brunex0 <brunomgmonteiro@gmail.com>
2021-04-28 18:58:10 +0100

    Correcao erros

--- backupCron.sh
+++ backupCron.sh
@@ -9,9 +9,4 @@ cd "$(crontab -l | cut -d " " -f 7)"
 #read default variables from the config file
 . ./src/readConfig.sh
 
-<<<<<<< HEAD
 . ./src/backupNowCron.sh
-=======
-cd "$(crontab -l | cut -d " " -f 7)"
-. ./src/backupNow.sh
->>>>>>> f491295eb7758716a957da2da4cec9aa0a5bb1ed
